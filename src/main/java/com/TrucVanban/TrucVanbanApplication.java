722011d00f47bae1468780ea3f53eed3aba3b17f
Le Chi Cong <lechicong9a2@gmail.com>
2026-06-13 15:56:24 +0700

    refactor: update application configuration and add dotenv support

--- src/main/java/com/TrucVanban/TrucVanbanApplication.java
+++ src/main/java/com/TrucVanban/TrucVanbanApplication.java
@@ -2,11 +2,17 @@ package com.TrucVanban;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvEntry;
 
 @SpringBootApplication
 public class TrucVanbanApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		for (DotenvEntry entry : dotenv.entries()) {
+			System.setProperty(entry.getKey(), entry.getValue());
+		}
 		SpringApplication.run(TrucVanbanApplication.class, args);
 	}
 
