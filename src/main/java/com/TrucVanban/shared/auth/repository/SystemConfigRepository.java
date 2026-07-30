2ea042b1b186c925b488dc8fb75b724e1ba9b4b7
Le Chi Cong <lechicong9a2@gmail.com>
2026-07-30 09:09:34 +0700

    feat: update API context path and refactor client simulator gateway URL

--- src/main/java/com/TrucVanban/shared/auth/repository/SystemConfigRepository.java
+++
@@ -1,7 +0,0 @@
-package com.TrucVanban.shared.auth.repository;
-
-import com.TrucVanban.shared.auth.entity.SystemConfig;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SystemConfigRepository extends JpaRepository<SystemConfig, Long> {
-}
