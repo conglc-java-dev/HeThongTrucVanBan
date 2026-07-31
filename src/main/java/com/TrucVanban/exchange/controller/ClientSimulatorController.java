c39801758c46fc2c45faddf2623161a7d7df3082
Nguyễn Mạnh Đức <nguyenduc2005qo@gmail.com>
2026-07-31 21:06:35 +0700

    feat(simulator): enhance file upload and signing functionality with new endpoints

--- src/main/java/com/TrucVanban/exchange/controller/ClientSimulatorController.java
+++ src/main/java/com/TrucVanban/exchange/controller/ClientSimulatorController.java
@@ -22,7 +22,7 @@ import java.util.List;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/v1/simulator")
+@RequestMapping("/simulator")
 @RequiredArgsConstructor
 public class ClientSimulatorController {
 
